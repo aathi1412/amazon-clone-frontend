3b6aa56cf0b5a9791a6e44681b757c96900aa361
aathi1412 <itsmeaathi@gmail.com>
2026-01-28 20:44:40 +0530

    learned more about classes and this

--- data/products.js
+++ data/products.js
@@ -61,7 +61,19 @@ class Clothing extends Product{
   }
 }
 
+/*
+const date = new Date();
+console.log(date.toLocaleTimeString());
 
+const obj = {
+  method : () => {
+    console.log(this);
+  }
+};
+
+obj.method();
+
+*/
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
