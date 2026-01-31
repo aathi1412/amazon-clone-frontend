c91567fa228dca27f27d7982bebe4be0413fc71b
aathi1412 <itsmeaathi@gmail.com>
2026-01-31 10:25:25 +0530

    learn promises and convert code to promises

--- data/cart-class.js
+++ data/cart-class.js
@@ -136,4 +136,14 @@ export const cart = new Cart('cart');
 // const buisnessCart = new Cart('buisness-cart');
 // console.log(buisnessCart);
 
+export function loadCart(fun){
+    const xhr = new XMLHttpRequest();
 
+    xhr.addEventListener('load', () => {
+        console.log(xhr.response);
+        fun();
+    });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
