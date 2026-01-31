2495cd0d3a535bf7ecb18c01acea8f08de25f51b
aathi1412 <itsmeaathi@gmail.com>
2026-01-31 09:50:27 +0530

    load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,5 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
-// import {loadFromStorage, cart} from '../../data/cart.js';
+import { loadProducts } from '../../data/products.js';
 import renderCheckoutHeader from '../../scripts/checkout/checkoutHeader.js';
 import {cart} from '../../data/cart-class.js';
 
@@ -12,6 +12,12 @@ describe('test suite: renderOrderSummary', () => {
 
         const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
         const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+    beforeAll((done) => {
+        loadProducts(() => {
+            done();
+        });
+    });
 
     beforeEach(() => {
 
@@ -84,7 +90,6 @@ describe('test suite: renderOrderSummary', () => {
            cart.cartItems[0].productId
         ).toEqual(productId2);
 
-        // expect(localStorage.setItem).toHaveBeenCalledWith('cart', '[]');
        
     });
 
